20ab13cc5709b5bb8586a41a91e9051f5d802ccd
Nitin Mokashi <56448363+nitin1498@users.noreply.github.com>
2022-09-17 01:34:07 +0530

    Update App.js
    added a food sound and incresed speed

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const foodSound = new Audio("music/food.mp3");
 const gameOverSound = new Audio("music/gameover.mp3");
 const moveSound = new Audio("music/move.mp3");
 const musicSound = new Audio("music/music.mp3");
-let speed = 2;
+let speed = 8;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
@@ -48,6 +48,7 @@ function gameEngine() {
 
 	if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
 		score += 1;
+        foodSound.play();
         if(score>hiscoreval){
             hiscoreval = score;
             localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
