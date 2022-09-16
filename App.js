488269dba5ffc8bbd2418fd62ffc261614d69580
nitin <nitinmokashi08@gmail.com>
2022-09-17 01:23:23 +0530

    added code to end game if snack colids with itself or colids into wall

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const foodSound = new Audio("music/food.mp3");
 const gameOverSound = new Audio("music/gameover.mp3");
 const moveSound = new Audio("music/move.mp3");
 const musicSound = new Audio("music/music.mp3");
-let speed = 10;
+let speed = 2;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
@@ -19,9 +19,34 @@ function main(ctime) {
 	gameEngine();
 }
 
+function isCollide(snake) {
+	for (let i = 1; i < snakeArr.length; i++) {
+		if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+			return true;
+		}
+	}
+	if (
+		snake[0].x >= 18 ||
+		snake[0].x <= 0 ||
+		snake[0].y >= 18 ||
+		snake[0].y <= 0
+	) {
+		return true;
+	}
+
+	return false;
+}
+
 function gameEngine() {
+	if (isCollide(snakeArr)) {
+		gameOverSound.play();  
+		inputDir = { x: 0, y: 0 };
+		alert("Game Over. Press any key to play again!");
+		snakeArr = [{ x: 13, y: 15 }];
+		score = 0;
+	}
+
 	if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
-		foodSound.play();
 		score += 1;
 		scoreBox.innerHTML = "Score: " + score;
 		snakeArr.unshift({
